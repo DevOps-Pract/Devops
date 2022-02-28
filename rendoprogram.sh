39a5a366f79d53f029e3b2c272b201436e25711b
DevOps-Pract <87228382+DevOps-Pract@users.noreply.github.com>
2022-02-28 10:59:38 +0000

    write some data about git

--- rendoprogram.sh
+++ rendoprogram.sh
@@ -11,3 +11,4 @@ c and c ++
 
 sucessfully completed
  rajeswari golla
+Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
